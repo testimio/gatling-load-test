d7205a8a20b441a7a32743c5f134a5724154c17f
Moshe Atlow <moshe@testim.io>
2021-03-18 00:18:31 +0200

    initial commit

--- src/test/scala/frontline/sample/BasicSimulation.scala
+++ src/test/scala/frontline/sample/BasicSimulation.scala
@@ -44,8 +44,6 @@ class BasicSimulation extends Simulation {
   //     exec{session => { tokenAPI = session("token").as[String]
   //     session}})
 
-var headers_10 = Map("Content-Type" -> """application/json""", "Authorization" -> "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNpOkVGbFU1Um1TbGNPOXJoc0t1TVBkIiwiaWF0IjoxNjE2MDE4MzkwLCJleHAiOjE2MTYwMjE5OTB9.DtP_bgDfPY6XaudNYLYCo8Pu7JMRmfKlVV7kMkDhink")
-
 
   val httpConf = http
     .baseUrl("https://services.testim.io")
@@ -55,8 +53,8 @@ var headers_10 = Map("Content-Type" -> """application/json""", "Authorization" -
     // exec { session => println(tokenAPI:String); session }
     // exec(session => session.set("token", tokenAPI))
     exec(http("lightweight")
-      .get("/result/lightweight/test")
-      .headers(headers_10)
+      .post("/result/lightweight/test")
+      .headers(Map("Content-Type" -> """application/json""", "Authorization" -> "Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNpOkVGbFU1Um1TbGNPOXJoc0t1TVBkIiwiaWF0IjoxNjE2MDE4MzkwLCJleHAiOjE2MTYwMjE5OTB9.DtP_bgDfPY6XaudNYLYCo8Pu7JMRmfKlVV7kMkDhink"))
       .check(status.in(200 to 210)))
       //.exec { session => println(session); session }
 
