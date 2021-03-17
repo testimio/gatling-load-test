36477da9e664f69d4a73fc37d82b5c0d04484e92
Moshe Atlow <moshe@testim.io>
2021-03-18 00:38:46 +0200

    initial commit

--- src/test/scala/frontline/sample/BasicSimulation.scala
+++ src/test/scala/frontline/sample/BasicSimulation.scala
@@ -58,7 +58,6 @@ class BasicSimulation extends Simulation {
  def  run() = {
     // exec { session => println("token print2"); session }
     // exec { session => println(tokenAPI:String); session }
-    feed(uuidfeeder)
     exec(session => session.set("token", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6ImNpOkVGbFU1Um1TbGNPOXJoc0t1TVBkIiwiaWF0IjoxNjE2MDE4MzkwLCJleHAiOjE2MTYwMjE5OTB9.DtP_bgDfPY6XaudNYLYCo8Pu7JMRmfKlVV7kMkDhink"))
     exec(http("lightweight")
       .post("/result/lightweight/test")
@@ -74,6 +73,7 @@ class BasicSimulation extends Simulation {
 
   val scn = scenario("scenario1")
     .pause(1)
+    .feed(uuidfeeder)
     .exec(run())
 
   setUp(
